9791df13946c0ed93cf268d4fa2e020f1e436ed4
Luis Porto <luis.porto@dafiti.com.br>
2023-02-05 12:11:25 -0300

    feat: adicionar parcelas repo resource

--- src/main/kotlin/com/luisow748/contasapg/repository/ParcelaRepository.kt
+++ src/main/kotlin/com/luisow748/contasapg/repository/ParcelaRepository.kt
@@ -2,6 +2,8 @@ package com.luisow748.contasapg.repository
 
 import com.luisow748.contasapg.domain.Parcela
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(path = "parcelas")
 interface ParcelaRepository: CrudRepository<Parcela, Int> {
 }
